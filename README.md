566b2597c3f3831337e5042e917d504b0ad615b2
lizthedeveloper <915684+lizTheDeveloper@users.noreply.github.com>
2024-05-29 21:24:40 +0000

    abstract the file reading function

--- README.md
+++ README.md
@@ -2,8 +2,22 @@
 MultiverseToDo
 
 - [ ] As a user my todos should be saved in a file so they persist when the program is not running
-- [ ] As a user I should be able to view my todos by typing something like `view`
+- [x] As a user I should be able to view my todos by typing something like `view`
 - [ ] As a user I should be able to add simple todos by typing something like `add "buy coffee creamer"`
 - [ ] As a user I should be able to mark todos as completed by typing something like `done 1`
 - [ ] As a user I should be able to unmark todos as completed by typing something like `undo 1`
 - [ ] As a user I should be able to delete todos by typing something like `delete 1`
+
+When I view my todos I should see something like this:
+```
+1: buy coffee creamer
+2: make list of todos
+3: finish todo app
+```
+
+## Schema
+a todo is:
+id = a number that refers to that todo easily
+todo_text = "buy coffee creamer"
+done = done / not done
+
--- main.py
+++ main.py
@@ -1,54 +1,54 @@
 
 
-def view_incomplete_todos():
-    ## write a function that will read todos.csv
-    todos_file = open("todos.csv", "r")
-    file_contents = todos_file.read()
-    # print(file_contents)
-    todos = file_contents.split("\n")
-    # print(todos)
-    ## for every line in todos
-    for todo in todos:
-        # print(todo)
-        todo_parts = todo.split(",")
-        # print(todo_parts)
-        if len(todo_parts) > 1:
-            done = todo_parts[2]
-            todo_text = todo_parts[1]
-            todo_id = todo_parts[0]
-            ## check to see if it's not done, if so:
-            if done == "0":
-                print(todo_id + ": " + todo_text)
-
-view_incomplete_todos()
-
-
-def complete_todo(completed_todo_id):
-    ## read todos.csv
-    todos_file = open("todos.csv", "r")
+def read_todos(filename):
+    todos_file = open(filename, "r")
     file_contents = todos_file.read()
     todos = file_contents.split("\n")
     todos_file.close()
-
-    ## open the file for writing
-    todos_file = open("todos.csv", "w")
-    ## find the todo
+    todos_in_parts = []
     for todo in todos:
         todo_parts = todo.split(",")
-        # print(todo_parts)
         if len(todo_parts) > 1:
-            done = todo_parts[2]
-            todo_text = todo_parts[1]
-            todo_id = todo_parts[0]
-            if completed_todo_id == todo_id:
-            ## edit the line, and change it to be done
-                todo_parts = [todo_id,todo_text,"1"]
-            put_back_together = ",".join(todo_parts)
-            print(todo_parts)
-            ## write to that file again
-            todos_file.write(put_back_together + "\n")
+            todo_dict = {
+                "done" : todo_parts[2],
+                "todo_text" : todo_parts[1],
+                "todo_id" : todo_parts[0]
+            }
+            todos_in_parts.append(todo_dict)
+    return todos_in_parts
 
+def view_incomplete_todos():
+    todos = read_todos('todos.csv')
+    for todo in todos:
+        ## check to see if it's not done, if so:
+        if todo["done"] == "0":
+            print(todo["todo_id"] + ": " + todo["todo_text"])
+
+def complete_todo(completed_todo_id):
+    todos = read_todos('todos.csv')
+    todos_file = open('todos.csv', 'w')
+    for todo in todos:        
+        done = todo["done"]
+        todo_text = todo["todo_text"]
+        todo_id = todo["todo_id"]
+        todo_parts = [todo_id,todo_text,done]
+        if completed_todo_id == todo_id:
+        ## edit the line, and change it to be done
+            todo_parts = [todo_id,todo_text,"1"]
+        put_back_together = ",".join(todo_parts)
+        ## write to that file again
+        todos_file.write(put_back_together + "\n")
+
+
+view_incomplete_todos()
 complete_todo('5')
+view_incomplete_todos()
 
+
+def add_todo(todo_text):
+    ## read the file
+    ## find the last ID number in the file
+    ## append the new todo to the file
+    pass
 
 
