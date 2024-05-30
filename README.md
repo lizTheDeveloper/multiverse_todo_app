b76762ae7efd0ea75d99f03dfe0942c313e9b0cb
lizthedeveloper <915684+lizTheDeveloper@users.noreply.github.com>
2024-05-30 17:54:52 +0000

    added decorator functions!

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # multiverse_todo_app
 MultiverseToDo
 
-- [ ] As a user my todos should be saved in a file so they persist when the program is not running
-- [x] As a user I should be able to view my todos by typing something like `view`
+- [x] As a user my todos should be saved in a file so they persist when the program is not running
+- [ ] As a user I should be able to view my todos by typing something like `view`
 - [ ] As a user I should be able to add simple todos by typing something like `add "buy coffee creamer"`
 - [ ] As a user I should be able to mark todos as completed by typing something like `done 1`
 - [ ] As a user I should be able to unmark todos as completed by typing something like `undo 1`
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 
+def read_and_write_todos(fn):
+    def wrapper(*args):
+        todos = read_todos("todos.csv")
+        result = fn(todos,*args)
+        write_todos(result,'todos.csv')
+        return result
+    return wrapper
+
 
 def read_todos(filename):
     todos_file = open(filename, "r")
@@ -15,40 +23,65 @@ def read_todos(filename):
                 "todo_id" : todo_parts[0]
             }
             todos_in_parts.append(todo_dict)
-    return todos_in_parts
+    return todos_in_parts[1:]
 
-def view_incomplete_todos():
-    todos = read_todos('todos.csv')
-    for todo in todos:
-        ## check to see if it's not done, if so:
-        if todo["done"] == "0":
-            print(todo["todo_id"] + ": " + todo["todo_text"])
 
-def complete_todo(completed_todo_id):
-    todos = read_todos('todos.csv')
-    todos_file = open('todos.csv', 'w')
+def write_todos(todos,filename):
+    todos_file = open(filename, 'w')
+    todos_file.write("id,todo_text,done\n")
     for todo in todos:        
-        done = todo["done"]
-        todo_text = todo["todo_text"]
-        todo_id = todo["todo_id"]
+        done = str(todo["done"])
+        todo_text = str(todo["todo_text"])
+        todo_id = str(todo["todo_id"])
         todo_parts = [todo_id,todo_text,done]
-        if completed_todo_id == todo_id:
-        ## edit the line, and change it to be done
-            todo_parts = [todo_id,todo_text,"1"]
         put_back_together = ",".join(todo_parts)
         ## write to that file again
         todos_file.write(put_back_together + "\n")
 
+@read_and_write_todos
+def view_incomplete_todos(todos):
+    for todo in todos:
+        ## check to see if it's not done, if so:
+        if todo["done"] == "0":
+            print(todo["todo_id"] + ": " + todo["todo_text"])
+    return todos
 
-view_incomplete_todos()
-complete_todo('5')
-view_incomplete_todos()
-
-
-def add_todo(todo_text):
-    ## read the file
+@read_and_write_todos
+def complete_todo(todos,completed_todo_id):
+    for todo in todos:        
+        if completed_todo_id == todo["todo_id"]:
+            todo["done"] = 1
+    
+    return todos
+            
+@read_and_write_todos
+def add_todo(todos,todo_text):
     ## find the last ID number in the file
+    for todo in todos:
+        last_id = int(todo["todo_id"])
+    print(last_id)
     ## append the new todo to the file
-    pass
+    new_todo = {
+        "todo_text":todo_text,
+        "todo_id": str(last_id + 1),
+        "done": "0"
+    }
+    todos.append(new_todo)
+    return todos
 
+
+
+# print("---- adding todo ---")
+# add_todo("finish todo app stuff")
+# view_incomplete_todos()
+# view_incomplete_todos()
+# complete_todo('5')
+# view_incomplete_todos()
+
+## stretch goal- how do we make it so the user can enter commands more than one time?
+
+user_input = input("> what would you like to do? view, add, do?")
+
+if user_input == "view":
+    view_incomplete_todos()
 
